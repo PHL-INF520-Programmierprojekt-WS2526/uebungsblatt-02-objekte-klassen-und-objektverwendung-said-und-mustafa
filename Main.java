0cceb6b4c8fa757c672797a3c36303184ed860b6
Orhan Ekici <orekici@hotmail.com>
2025-11-11 07:49:57 +0100

    Add files via upload
    petadoption

--- Main.java
+++ Main.java
@@ -1,50 +1,42 @@
-package de.phl.programmingproject.enrollmentsystem;
+package de.phl.programmingproject.petadoption;
+
+import java.util.Date;
+
 
 public class Main {
+
+
+
     public static void main(String[] args) {
+        
+        Pet pet = new Pet("Buddy", 3, "Labrador Retriever");
 
-        Student student = new Student("John Doe", "12345");
-        Course course1 = new Course("Introduction to Computer Science");
+        
+        Adopter adopter = new Adopter("Jane Smith", "555-555-5555", "janesmith@email.com");
 
-        System.out.println("=== Schritt 3: Anfangsinformationen ===");
-        System.out.println(student.getInfo());
-        System.out.println(course1.getInfo());
+        
+         System.out.println("Haustier-Informationen:");
+           System.out.println(pet.getInfo());
         System.out.println();
 
-        Enrollment enrollment1 = student.enroll(course1);
-
-        System.out.println("=== Schritt 5: Nach der Einschreibung ===");
-        System.out.println(student.getInfo());
-        System.out.println(course1.getInfo());
+        
+           System.out.println("Adoptierenden-Informationen:");
+          System.out.println(adopter.getInfo());
         System.out.println();
 
-        System.out.println("=== Schritt 6: Enrollment-Informationen ===");
-        System.out.println(enrollment1.getInfo());
+        
+        System.out.println("Wurde das Haustier adoptiert? " + pet.isAdopted());
+          System.out.println();
+
+        
+        Adoption adoption = pet.adopt(adopter, new Date());
+
+        
+           System.out.println("Adoptions-Informationen:");
+        System.out.println(adoption.getInfo());
         System.out.println();
 
-        enrollment1.setGrade(4.0);
-
-        System.out.println("=== Schritt 8: Aktualisierte Enrollment-Informationen ===");
-        System.out.println(enrollment1.getInfo());
-        System.out.println();
-
-        Course course2 = new Course("Object-Oriented Programming");
-        student.enroll(course2);
-
-        System.out.println("=== Schritt 11: Alle Kurse des Studenten ===");
-        student.getCourses().forEach(c -> System.out.println(" - " + c.getName()));
-        System.out.println();
-
-        System.out.println("=== Schritt 12: Studierende im Kurs '" + course1.getName() + "' ===");
-        course1.getStudents().forEach(s -> System.out.println(" - " + s.getName()));
-        System.out.println();
-
-        student.drop(course1);
-
-        System.out.println("=== Schritt 14: Studierende nach dem Austragen ===");
-        course1.getStudents().forEach(s -> System.out.println(" - " + s.getName()));
-        System.out.println();
-
-        System.out.println("=== Fertig! Das Enrollment-System funktioniert korrekt. ===");
+        
+        System.out.println("Wurde das Haustier adoptiert? " + pet.isAdopted());
     }
 }
