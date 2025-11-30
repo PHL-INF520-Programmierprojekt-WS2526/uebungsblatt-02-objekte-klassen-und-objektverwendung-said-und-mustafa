c122037b715a68ff7f89f59614c446c79b12cc05
arslanbabam <arslanbaba.mustafa@gmail.com>
2025-11-30 21:25:14 +0100

    Refactor Adoption class and update getInfo method
    Refactored Adoption class to use non-final fields and updated getInfo method to format output in German.

--- src/main/java/de/phl/programmingproject/petadoption/Adoption.java
+++ src/main/java/de/phl/programmingproject/petadoption/Adoption.java
@@ -1,26 +1,21 @@
 package de.phl.programmingproject.petadoption;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * The adoption of a pet by an adopter at a given date.
- */
 public class Adoption {
-
-    private final Pet pet;
-    private final Adopter adopter;
-    private final Date date;
+    private Pet pet;
+    private Adopter adopter;
+    private Date date;
 
     public Adoption(final Pet pet, final Adopter adopter, final Date date) {
-        if (pet == null || adopter == null || date == null) {
-            throw new IllegalArgumentException("Pet, adopter and date must not be null");
-        }
         this.pet = pet;
         this.adopter = adopter;
         this.date = date;
     }
 
     public String getInfo() {
-        return String.format("Pet: %s, Adopter: %s, Date: %s", pet.getInfo(), adopter.getInfo(), date);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        return "Haustier: " + pet.getName() + ", Adoptierende: " + adopter.getName() + ", Datum: " + sdf.format(date);
     }
 }
